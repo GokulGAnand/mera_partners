14742fb497c23e1bf8ccb5f0ef4587460a267ec2
vishnu <vishnuvyloram@gmail.com>
2024-05-07 11:37:17 +0530

    previous images some parts and next images parts are showing issue fixed

--- lib/widgets/custom_slider.dart
+++ lib/widgets/custom_slider.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
@@ -7,7 +6,7 @@ import '../utils/colors.dart';
 import '../utils/images.dart';
 
 class CustomSlider extends StatelessWidget {
-   const CustomSlider({
+  const CustomSlider({
     super.key,
     required this.sliderImage,
     required this.pageSliderController,
@@ -24,6 +23,9 @@ class CustomSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Set initialPage to ensure only one page is visible at a time
+     pageSliderController.value = PageController(initialPage: 0);
+
     return GestureDetector(
       onHorizontalDragUpdate: (details) {
         if (details.delta.dx > 0) {
@@ -46,93 +48,96 @@ class CustomSlider extends StatelessWidget {
           }
         }
       },
-      child: Obx(() => SizedBox(
-        height: height,
-        child: Stack(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: height,
-              child: PageView.builder(
-                controller: pageSliderController.value,
-                onPageChanged: (index) {
-                  activePage.value = index;
-                  pageSliderController.value.animateToPage(
-                    activePage.value,
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeIn,
-                  );
-                },
-                itemBuilder: (_, index) {
-                  return Image.network(sliderImage[index],
+      child: Obx(
+            () => SizedBox(
+          height: height,
+          child: Stack(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: height,
+                child: PageView.builder(
+                  controller: pageSliderController.value,
+                  onPageChanged: (index) {
+                    activePage.value = index;
+                    pageSliderController.value.animateToPage(
+                      activePage.value,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
+                  },
+                  itemBuilder: (_, index) {
+                    return Image.network(
+                      sliderImage[index],
                       fit: BoxFit.cover,
                       errorBuilder: (context, error, stackTrace) {
                         return SvgPicture.asset(MyImages.loadingCar);
-                      }, frameBuilder:
-                          (context, child, frame, wasSynchronouslyLoaded) {
+                      },
+                      frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
                         return child;
-                      }, loadingBuilder:
-                          (context, child, loadingProgress) {
+                      },
+                      loadingBuilder: (context, child, loadingProgress) {
                         if (loadingProgress == null) {
                           return child;
                         } else {
                           return SvgPicture.asset(MyImages.loadingCar);
                         }
-                      });
-                },
-                itemCount: sliderImage.length,
+                      },
+                    );
+                  },
+                  itemCount: sliderImage.length,
+                ),
               ),
-            ),
-            if (showBlackOpacity)
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: height,
-                  decoration: ShapeDecoration(
-                    gradient: LinearGradient(
-                      begin: const Alignment(0.00, -0.00),
-                      end: const Alignment(0, 1),
-                      colors: [
-                        Colors.black.withOpacity(0),
-                        Colors.black
-                      ],
-                    ),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(8),
-                        topRight: Radius.circular(8),
+              if (showBlackOpacity)
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: height,
+                    decoration: ShapeDecoration(
+                      gradient: LinearGradient(
+                        begin: const Alignment(0.00, -0.00),
+                        end: const Alignment(0, 1),
+                        colors: [
+                          Colors.black.withOpacity(0),
+                          Colors.black
+                        ],
+                      ),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8),
+                          topRight: Radius.circular(8),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            Positioned(
-              bottom: 12,
-              left: MediaQuery.of(context).size.width * 0.39,
-              child: Obx(
-                    () => SizedBox(
-                  width: 41,
-                  height: 4.56,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(
-                      5,
-                          (index) => GestureDetector(
-                        onTap: () => pageSliderController.value
-                            .animateToPage(
-                          index,
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.easeIn,
-                        ),
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 2.0),
-                          child: Icon(
-                            Icons.circle,
-                            size: 6.0,
-                            color: activePage.value == index
-                                ? MyColors.white
-                                : MyColors.grey,
+              Positioned(
+                bottom: 12,
+                left: MediaQuery.of(context).size.width * 0.39,
+                child: Obx(
+                      () => SizedBox(
+                    width: 41,
+                    height: 4.56,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        5,
+                            (index) => GestureDetector(
+                          onTap: () => pageSliderController.value.animateToPage(
+                            index,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 2.0),
+                            child: Icon(
+                              Icons.circle,
+                              size: 6.0,
+                              color: activePage.value == index
+                                  ? MyColors.white
+                                  : MyColors.grey,
+                            ),
                           ),
                         ),
                       ),
@@ -140,10 +145,10 @@ class CustomSlider extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
