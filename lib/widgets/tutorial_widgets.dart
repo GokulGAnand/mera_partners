6f2a10e76e1d0e55550ce886b0312c1bdc21a9c2
vishnu <vishnuvyloram@gmail.com>
2024-05-23 12:45:10 +0530

    tutorial widget swiping issue

--- lib/widgets/tutorial_widgets.dart
+++ lib/widgets/tutorial_widgets.dart
@@ -23,104 +23,109 @@ class TutorialWidgets extends StatelessWidget {
   Widget build(BuildContext context) {
     TutorialScreenViewModel controller = Get.find<TutorialScreenViewModel>();
     final size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          // SizedBox(height: Dimens.standard_2),
-          Container(
-            height: size.height * 0.3,
-            width: size.width * 0.8,
-            // alignment: Alignment.bottomCenter,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(6), color: MyColors.lightGrey),
-            child: Container(
+    return GestureDetector(
+      onHorizontalDragStart: (_){
+        
+      },
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // SizedBox(height: Dimens.standard_2),
+            Container(
               height: size.height * 0.3,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(image ?? ''),
+              width: size.width * 0.8,
+              // alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(6), color: MyColors.lightGrey),
+              child: Container(
+                height: size.height * 0.3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(image ?? ''),
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: size.height * 0.35,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              // color: Colors.white,
-            ),
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: Dimens.standard_20),
-                Text(
-                  '$title',
-                  style: MyStyles.black18500,
-                ),
-                SizedBox(height: Dimens.standard_20),
-                Padding(
-                  padding: const EdgeInsets.only(left: 5),
-                  child: Text.rich(
-                    TextSpan(
-                        text: '$subtitle',
-                        style: MyStyles.black15400,
-                        children: [
-                          TextSpan(
-                            text: " ${colourTitle ?? ''}",
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w700,
-                              color: colour,
-                            ),
-                          ),
-                          const TextSpan(text: MyStrings.color, style: MyStyles.black15400),
-                        ]),
-                    textAlign: TextAlign.center,
+            Container(
+              height: size.height * 0.35,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                // color: Colors.white,
+              ),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: Dimens.standard_20),
+                  Text(
+                    '$title',
+                    style: MyStyles.black18500,
                   ),
-                ),
-                SizedBox(height: Dimens.standard_20),
-                Obx(() => Align(
-                  alignment: const Alignment(0, 0.10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List<Widget>.generate(
-                      4,
-                          (index) => Container(
-                        margin: const EdgeInsets.all(1),
-                        width: 24,
-                        height: 10,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: controller.page.value == index ? MyColors.kPrimaryColor : MyColors.lightGrey,
+                  SizedBox(height: Dimens.standard_20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Text.rich(
+                      TextSpan(
+                          text: '$subtitle',
+                          style: MyStyles.black15400,
+                          children: [
+                            TextSpan(
+                              text: " ${colourTitle ?? ''}",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                color: colour,
+                              ),
+                            ),
+                            const TextSpan(text: MyStrings.color, style: MyStyles.black15400),
+                          ]),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: Dimens.standard_20),
+                  Obx(() => Align(
+                    alignment: const Alignment(0, 0.10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List<Widget>.generate(
+                        4,
+                            (index) => Container(
+                          margin: const EdgeInsets.all(1),
+                          width: 24,
+                          height: 10,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: controller.page.value == index ? MyColors.kPrimaryColor : MyColors.lightGrey,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                )),
-                SizedBox(height: Dimens.standard_20),
-                Obx(
-                      () => CustomElevatedButton(
-                    onPressed: () {
-                      if (!buttonPressed) {
-                        log('Current Page: ${controller.page.value}');
-                        if (controller.page.value == 3) {
-                          Navigator.of(context).pop();
-                        } else {
-                          controller.indicator.jumpToPage(
-                            controller.page.value.toInt() + 1,
-                          );
+                  )),
+                  SizedBox(height: Dimens.standard_20),
+                  Obx(
+                        () => CustomElevatedButton(
+                      onPressed: () {
+                        if (!buttonPressed) {
+                          log('Current Page: ${controller.page.value}');
+                          if (controller.page.value == 3) {
+                            Navigator.of(context).pop();
+                          } else {
+                            controller.indicator.jumpToPage(
+                              controller.page.value.toInt() + 1,
+                            );
+                          }
+                          controller.update();
+                          buttonPressed = true;
                         }
-                        controller.update();
-                        buttonPressed = true;
-                      }
-                    },
-                    textColor: MyColors.white,
-                    buttonText: controller.page.value == 3 ? MyStrings.startBiding : MyStrings.next,
-                  ),
-                )
-              ],
+                      },
+                      textColor: MyColors.white,
+                      buttonText: controller.page.value == 3 ? MyStrings.startBiding : MyStrings.next,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
