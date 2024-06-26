23dbd01d64e549b3f8fe257a7f8cff46701a4c42
ranjitha57 <ranjithaarunagiri21@gmail.com>
2024-06-26 16:03:20 +0530

    image view screen issue fixed

--- lib/view/gallery/image_view_screen.dart
+++ lib/view/gallery/image_view_screen.dart
@@ -6,8 +6,6 @@ import 'package:mera_partners/widgets/common_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../utils/images.dart';
-
 class ImageViewScreen extends StatelessWidget {
   const ImageViewScreen({super.key});
 
@@ -21,50 +19,59 @@ class ImageViewScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 16,right: 16,bottom: 8,),
-            child: Row(
-              children: [
-                Expanded(
-                  child: image["subtitle"] != null && image["subtitle"].isNotEmpty
-                      ? Text(
-                    image["image_title"] + ' (${image["subtitle"]})',
+          image["subtitle"] == null || image["subtitle"].isEmpty
+              ? Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    image["image_title"],
                     style: MyStyles.white16700,
-                  )
-                      : const SizedBox(),
+                  ),
                 )
-              ],
-            ),
-          ),
-          Expanded(
-            child: InteractiveViewer(
-              minScale: 0.1,
-              maxScale: 4.0,
-              clipBehavior: Clip.none,
-              child: Center(
-                child: Image.network(
-                  image["image"],
-                  fit: BoxFit.contain,
-                  errorBuilder: (context, error, stackTrace) {
-                    return SvgPicture.asset(MyImages.loadingCar);
-                  },
-                  frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-                    return child;
-                  },
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if (loadingProgress == null) {
-                      return child;
-                    } else {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
+              : Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                    bottom: 8,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: image["subtitle"] != null && image["subtitle"].isNotEmpty
+                            ? Text(
+                                image["image_title"] + ' (${image["subtitle"]})',
+                                style: MyStyles.white16700,
+                              )
+                            : const SizedBox(),
+                      )
+                    ],
+                  ),
                 ),
+          InteractiveViewer(
+            minScale: 0.1,
+            maxScale: 4.0,
+            clipBehavior: Clip.none,
+            child: Center(
+              child: Image.network(
+                image["image"],
+                fit: BoxFit.contain,
+                errorBuilder: (context, error, stackTrace) {
+                  return SvgPicture.asset(MyImages.loadingCar);
+                },
+                frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+                  return child;
+                },
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
               ),
             ),
           ),
-
         ],
       ),
     );
